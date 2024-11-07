24d215e2e7b0a64ecd6020f8a5b7d0e2831a1fb7
Xavier Favier <xaxou13@gmail.com>
2024-11-07 11:11:31 +0100

    first commit

---
+++ src/test/java/org/example/ecoapp/EcoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
